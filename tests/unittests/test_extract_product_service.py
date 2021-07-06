5139fa827c5a6954d382521652e720896d7a610d
Vitor Moreira <vitor.moreira@syngenta.com>
2021-07-05 23:07:43 -0300

    adding test product without price

--- tests/unittests/test_extract_product_service.py
+++ tests/unittests/test_extract_product_service.py
@@ -10,10 +10,13 @@ from tests.utils_test import UtilsTest
 class TestExtractProductService(unittest.TestCase):
 
     def setUp(self):
-        self.source_dto_file_path1 = UtilsTest.get_resource(os.path.join("sources", "amazon_chromebook.json"))
+        self.source_dto_file_path1 = UtilsTest.get_resource(os.path.join("sources", "amazon_product_page.json"))
         self.page_file_path1 = UtilsTest.get_resource(os.path.join("pages", "amazon_chromebook.html"))
 
-    def test_extract_product1(self):
+        self.source_dto_file_path2 = UtilsTest.get_resource(os.path.join("sources", "amazon_product_page.json"))
+        self.page_file_path2 = UtilsTest.get_resource(os.path.join("pages", "amazon_playstation5.html"))
+
+    def test_extract_product_with_price(self):
         source_dto_file1 = open(self.source_dto_file_path1, "r")
         source_dto = json.load(source_dto_file1)
         source_dto_file1.close()
@@ -29,3 +32,20 @@ class TestExtractProductService(unittest.TestCase):
         self.assertEqual(1, len(products))
         self.assertEqual("Chromebook Acer R721T-488H AMD A4-9120C 4GB 11,6&quot; Chrome OS", products[0].name)
         self.assertEqual("R$1.999,00", products[0].price)
+
+    def test_extract_product_without_price(self):
+        source_dto_file2 = open(self.source_dto_file_path2, "r")
+        source_dto = json.load(source_dto_file2)
+        source_dto_file2.close()
+
+        source = Source.instantiate_from_dto(source_dto)
+
+        page_file2 = open(self.page_file_path2, "r")
+        page2 = page_file2.read()
+        page_file2.close()
+
+        products = ExtractProductService.extract_product_from_html(page2, source.template)
+
+        self.assertEqual(1, len(products))
+        self.assertEqual("Console PlayStation 5 - Digital Edition", products[0].name)
+        self.assertIsNone(products[0].price)
